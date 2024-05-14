612d78466e8767cf8a2851cfd98af1edae18d5e6
bibhuExamsOfBharat <bibhu.examsofbharat@gmail.com>
2024-05-14 14:19:16 +0530

    dto and service layer added

--- agni/src/main/java/com/examsofbharat/bramhsastra/agni/AgniApplication.java
+++ agni/src/main/java/com/examsofbharat/bramhsastra/agni/AgniApplication.java
@@ -2,8 +2,10 @@ package com.examsofbharat.bramhsastra.agni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.examsofbharat.bramhsastra"})
+@EnableScheduling
 public class AgniApplication {
 
     public static void main(String[] args) {
